da5855b5765658e47148d717e89025968ec7a8f7
tristiangarvin <tristian.garvin@okstate.edu>
2022-09-14 07:47:59 -0500

    yeet

--- app.py
+++ app.py
@@ -16,10 +16,17 @@ df['Episodes'] = df.groupby(['Title']).cumcount() + 1
 
 df['Average'] = df.groupby('Title')['Rating'].transform('mean')
 
+dfsh = df.drop_duplicates(subset=['Title'])
+
+dfsh = dfsh.sort_values(by='Average', ascending=False)
+
+dfsh
+
 dropdown_show = dcc.Dropdown(
     id="dropdown-show",
     options=[{"label": show, "value": show}
-             for show in sorted(df.Title.unique())],
+             for show in dfsh.Title.unique()],
+             #for show in sorted(df.Title.unique())],
     multi=False,
     value='Breaking Bad',
     clearable=False,
@@ -28,9 +35,9 @@ dropdown_show = dcc.Dropdown(
 app.layout = dbc.Container([
     dbc.Row([
         dbc.Col([
-            html.H1('IMDB Ratings'),
+            html.H1('IMDB Ratings',),
             dropdown_show,
-            dcc.Graph(id='ratings-chart', figure={})
+            dcc.Graph(id='ratings-chart', figure={},  className="pt-5", responsive=True),
         ], lg=12)
     ],),
 ],)
@@ -39,9 +46,33 @@ app.layout = dbc.Container([
               Input('dropdown-show', 'value'))
 def update_layout(show):
     df2 = df[df['Title'] == show]
-    fig = px.line(df2, x='Episodes', y='Rating', markers=True, line_shape='spline',)
-    fig.update_yaxes(range=[1,10], dtick=1)
-    fig.update_traces(textposition="bottom right")
+    fig = px.line(df2, x='Episodes', y='Rating', markers=True, line_shape='spline', title="IMDb Rating by Episode",)
+    fig.update_yaxes(range=[0,10], dtick=1, showgrid=False, zeroline=True, ticksuffix = "  ")
+    fig.update_xaxes(showgrid=False, zeroline=True, rangemode="tozero")
+    fig.update_traces(textposition="bottom center", line_color='#F5C518')
+
+
+    high = df2[df2.Rating == df2.Rating.max()]
+    low = df2[df2.Rating == df2.Rating.min()]
+
+    highscore = high['Rating'].iloc[0].item()
+    lowscore = low['Rating'].iloc[0].item()
+
+    fig.add_annotation(text='Highest Rated Episode: ' + str(highscore), y=high.iloc[-1]['Rating'], x=high.iloc[-1]['Episodes'], arrowcolor='white')
+    fig.add_annotation(text='Lowest Rated Episode: ' + str(lowscore), y=low.iloc[0]['Rating'], x=low.iloc[0]['Episodes'], arrowcolor='white', font=dict(color="#E85669"), ay=30)
+
+    fig.update_layout(
+            {
+            'paper_bgcolor': 'rgba(0,0,0,0)',
+            'plot_bgcolor': 'rgba(0,0,0,0)',
+            },
+            showlegend=False,
+            autosize=False,
+            margin={'t':60, 'l': 20, 'b': 20, 'r': 20},
+            font_color="white",
+            hovermode="x",
+        )
+
 
     return fig
 
