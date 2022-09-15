a764677f00c9a3f46d63b58382f1584d5bd189cd
Tristian Garvin <tristian.garvin@okstate.edu>
2022-09-15 16:31:30 -0500

    Updates to UI and added chart info.

--- scrape.py
+++ scrape.py
@@ -8,18 +8,15 @@ sheet=wb.active
 
 sheet.append(["Title", "Rating"])
 
-
 url ="https://www.imdb.com/chart/toptv/"
 req=requests.get(url)
 soup=BeautifulSoup(req.text,"html.parser")
 linked=soup.find("tbody",class_="lister-list")
 rows=linked.find_all("tr")
-for i in rows:
-    td=i.select_one("td.titleColumn > a").text.replace("\n"," ")
-    tdd=i.find("td",class_="ratingColumn imdbRating").text.replace("\n"," ")
-    print(td)
-    print(tdd)
-    sheet.append([td,tdd])
-wb.save("series_overall_ratings.xlsx")  
-print(wb.sheetnames)
 
+with open('show_overall.csv', 'a', encoding='utf-8') as csv:
+    csv.write('Title,Rating_Overall\n')
+    for i in rows:
+        td=i.select_one("td.titleColumn > a").text.replace("\n"," ")
+        tdd=i.find("td",class_="ratingColumn imdbRating").text.replace("\n"," ")
+        csv.write(td + ',' + tdd + '\n')
